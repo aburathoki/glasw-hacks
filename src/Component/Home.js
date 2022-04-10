829febc39144c7c8b83d33ecce36c4cbfd9318ae
Vaibhav <vaibhav@Vaibhavs-MacBook-Air.local>
2022-04-10 15:55:34 +0530

    changes

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -51,6 +51,7 @@ const Home = () => {
                 src={vision}
                 alt="vision"
                 className="mt-4 vision-image"
+                style={{ width: window.screen.width < 700 && "90vw" }}
               ></img>
             </div>
           </div>
@@ -65,6 +66,7 @@ const Home = () => {
                   src={delivery}
                   alt="delivery"
                   className="delivery-image "
+                  style={{ width: window.screen.width < 700 && "70vw" }}
                 ></img>
               </div>
             </div>
